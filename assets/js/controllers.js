d9fc5cca3429b77541c0c9c0bd1aa2c9d8a37869
Pius Wanjiru <piusmuriithi1@gmail.com>
2016-09-28 11:56:12 +0300

    adds navigation on the details partial

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -11,5 +11,18 @@ favouriteControllers.controller('DetailsController', ['$scope', '$http', '$route
 	$http.get('assets/js/data.json').success(function(data){
 		$scope.artists = data;
 		$scope.whichItem = $routeParams.itemId;
+
+		if ($routeParams.itemId > 0) {
+			$scope.prevItem = Number($routeParams.itemId)-1;
+		}else{
+			$scope.prevItem = $scope.artists.length-1;
+		}
+
+		if ($routeParams.itemId > $scope.artists.length-1) {
+			$scope.nextItem = Number($routeParams.itemId)+1;
+		}else{
+			$scope.nextItem = 0;
+		}
+
 	});
 }]);
