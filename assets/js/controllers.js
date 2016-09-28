9f60a075db5f57242bef36bac13047a30c088be6
Pius Wanjiru <piusmuriithi1@gmail.com>
2016-09-28 10:28:59 +0300

    adds more filters

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,5 +3,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', ['$scope', '$http', function MyController($scope, $http){
 	$http.get('assets/js/data.json').success(function(data){
 		$scope.artists = data;
+		$scope.artistOrder = 'name';
 	});
 }]);
